14be289276a018d57ffa42574a54f2424967f715
U-robert-PC\robert <robert@robert-PC.(none)>
2011-10-30 22:46:42 -0700

    =first commit for robert

---
+++ audio.h
@@ -0,0 +1,21 @@
+//
+// Test file to commit
+// TODO: add code here to load some audio
+//
+#ifndef __AUDIO_H__
+#define __AUDIO_H__
+
+#include "SDL.h"
+#include "SDL_mixer.h"
+
+const char *audio_filename;
+
+//or some shit
+void load_file(const char *);
+//
+// Audio code for 
+//
+//
+
+
+#endif //__AUDIO_H__
