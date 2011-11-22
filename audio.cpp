996e39deadbef52db67834d75feadfcf7fb15e45
Robert Karl <robertkarljr@gmail.com>
2011-11-22 15:31:44 -0800

    Audio API updated to include stereo. Makefile modified

--- audio.cpp
+++ audio.cpp
@@ -3,18 +3,69 @@
 #include <cassert>
 #include "ParamReader.h"
 
-AudioManager::AudioManager() {}
+AudioManager::AudioManager() 
+{
+    // Load in all small sound files, like attack noise
+    // Sample code for loading and playing a sound file
+    // played through the left speaker follows 
+    /*std::string fname = "sfx/ko-01.wav";
+    sf::SoundBuffer *sb = new sf::SoundBuffer();
+    sb->LoadFromFile(fname);
+    s2->SetPosition(-1, 0, 0);
+    s1->SetAttenuation(100);*/
+}
+#define D1 25.0f
+#define D2 300.0f
+#define V1 25.0f
+#define V2 100.0f
 
 void AudioManager::playSound(const std::string &fname)
 {
-    std::string fullPath = "sfx/" + fname + ".aif";
-    sf::Music *music = new sf::Music();
-    assert(music);
-    assert(music->OpenFromFile(fullPath));
-    music->Play();
+    // check if the file has already been loaded in
+    if (buffers_.count(fname) != 1) {
+        sf::SoundBuffer *sb = new sf::SoundBuffer();
+        assert(sb);
+        sb->LoadFromFile("sfx" + fname + ".aif");
+        buffers_[fname] = sb;
+    }
+    sf::Sound *s = new sf::Sound();
+    s->SetBuffer(*buffers_[fname]);
+    s->Play();
+    currentSounds_.push_back(s);
 
-    currentSounds_.push_back(music);
-
+}
+void AudioManager::playSound(const std::string &fname, 
+        glm::vec2 pos,
+        double damage)
+{
+    assert(damage >= 0);
+    double panningFactor = -2;
+    if (damage > 300) damage = 300;
+    // check if the file has already been loaded in
+    if (buffers_.count(fname) != 1) {
+        sf::SoundBuffer *sb = new sf::SoundBuffer();
+        assert(sb);
+        sb->LoadFromFile("sfx" + fname + ".aif");
+        buffers_[fname] = sb;
+    }
+    sf::Sound *s = new sf::Sound();
+    s->SetBuffer(*buffers_[fname]);
+    s->SetPosition(panningFactor, 0, 0);
+    assert(panningFactor >= -1 && panningFactor <= 1);
+    double vol;
+    if (damage > 0 && damage < D1) {
+        vol = V1;
+    }
+    else if (damage < D2) {
+        vol = V1 + ((V2 - V1) / (D2 - D1)) * (damage - D1);
+    }
+    else if (damage > D2) {
+        vol = V2;
+    }
+    assert(vol >= V1 && vol <= V2);
+    s->SetVolume(vol);
+    // play that shit
+    s->Play();
 }
 
 void AudioManager::setSoundtrack(const std::string &file)
@@ -43,7 +94,7 @@ AudioManager* AudioManager::get()
 
 void AudioManager::update(float dt) 
 {
-    std::vector<sf::Music *>::iterator it;
+    std::vector<sf::Sound *>::iterator it;
     for (it = currentSounds_.begin(); it != currentSounds_.end(); )
     {
         bool notplaying = (*it)->GetStatus() == sf::Sound::Stopped;
--- audio.h
+++ audio.h
@@ -1,6 +1,18 @@
 #pragma once
 
 #include <SFML/Audio.hpp>
+#include <map>
+#include <string>
+#include <glm/glm.hpp>
+
+//
+// SFML Notes
+// SFML uses OpenAL, which will NOT use spatialization features
+// on audio files that contain more than one channel.
+//
+// YOU MUST USE MONO AUDIO FILES TO USE PANNING! 
+// you have been warned.
+//
 
 class AudioManager
 {
@@ -10,7 +22,19 @@ public:
     // Play an audio file (for example, on being hit with an attack)
     // (Calling object is responsible for building the identifier, 
     // not the full filename)
-    void playSound(const std::string &audioIdentifier);
+    //
+    // @param fighterLocation: Where is the fighter in world coords
+    //                       We will figure out the rest
+    //                       TODO: change this
+    //
+    // @param damage: at 300 damage, sound will be saturated, or full volume.
+    // 
+    // Intended use: playing small sounds like KO or attack noises.
+    void playSound(const std::string &audioIdentifier,
+            glm::vec2 loc, // between -1 and 1
+            double damage); // between 0, ~300 usually
+
+    void playSound(const std::string &audioID);
 
     // Loads the filename as the soundtrack - does not start playing it until
     // startSoundtrack() is called.
@@ -27,10 +51,14 @@ public:
 private:
     AudioManager();
 
-    // list of sounds currently playing. we need to periodically check 
-    // this list to not leak memory.
-    std::vector<sf::Music *> currentSounds_;
+    // List of all buffers representing small sounds, like attack noises
+    std::map<std::string, sf::SoundBuffer *>buffers_;
+
+    // List of small sounds playing 
+    // Each sound will point to a buffer in soundBuffers_.
+    std::vector<sf::Sound *> currentSounds_;
 
     sf::Music soundtrack_;
 };
+
 
