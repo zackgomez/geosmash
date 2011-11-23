d853d1ba3ffad9a9ffccadcd3f8b2cc5fcd4f134
Robert Karl <robertkarljr@gmail.com>
2011-11-22 17:46:45 -0800

    Camera Manager changes

--- CameraManager.cpp
+++ CameraManager.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include "CameraManager.h"
 
+#define MINY -75.0
+#define MINX (-100.0f)
+#define MAXX (100.0f)
+
 CameraManager::CameraManager()
 {}
 
 void CameraManager::update(float dt, const std::vector<Fighter *> &fighters)
 {
-   
-    
     updateTarget_(fighters);
     updateCurrent_();
-
-
+    //setCamera(glm::vec3(-100, 100, 500));
 }
 
 void CameraManager::setCurrent(const glm::vec3 &v)
@@ -29,6 +30,14 @@ void CameraManager::updateTarget_(const std::vector<Fighter *> &fighters) {
 
     std::cout << "x " << totalPos.x
               << ", y " << totalPos.y << std::endl;
+    if (totalPos.y < MINY) {
+        totalPos.y = MINY;
+    }
+    if (totalPos.x < MINX) 
+        totalPos.x = MINX;
+    if (totalPos.x > MAXX)
+        totalPos.x = MAXX;
+    target_ = glm::vec3(totalPos.x, totalPos.y, 425);
 
 }
 
@@ -36,6 +45,8 @@ void CameraManager::updateTarget_(const std::vector<Fighter *> &fighters) {
 
 void CameraManager::updateCurrent_() {
 
+    current_ = target_;
+    setCamera(current_);
 
 }
 
