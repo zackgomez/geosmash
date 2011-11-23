c1314a858cc4c72f6282fd96b0cb3c6f292fe017
Robert Karl <robertkarljr@gmail.com>
2011-11-23 12:10:00 -0800

    Some progress on particles

--- ParticleManager.cpp
+++ ParticleManager.cpp
@@ -7,9 +7,54 @@ ParticleManager::ParticleManager()
 
 void ParticleManager::render(float dt)
 {
+    // first create new particles
+    for (int i = 0; i < emitters.size(); i++) 
+    {
+        Emitter* e = emitters[i];
+        int numNewParts = emitters[i]->rate_ * dt;
+        for (int j = 0; j < numNewParts; j++)
+        {
+            Particle *p = new Particle();
+            p->loc = e->loc_;
+            p->t = e->lifetime_;
+            p->vel = e->vel_;
+            particles.push_back(p);
+        }
+
+    }
+
+    // then update old particles
+    for (unsigned i = 0; i < particles.size(); i++) 
+    {
+        
+        //Particle *p = particles[i];
+    }
+ 
+    // finally draw existing particles
 }
 
-ParticleManager* ParticleManager::get() {
+ParticleManager* ParticleManager::get()
+{
     static ParticleManager pm;
     return &pm;
 }
+
+Emitter* Emitter::setParticleLifetime(float l) 
+{ 
+    lifetime_ = l; 
+    return this;
+}
+
+
+Emitter* Emitter::setLocation(glm::vec3 l) 
+{ 
+    loc_ = l; 
+    return this;
+}
+
+Emitter* Emitter::setOutputRate(float r)
+{
+    rate_ = r;
+    return this;
+}
+
--- ParticleManager.h
+++ ParticleManager.h
@@ -3,7 +3,33 @@
 #include "glutils.h"
 #include <vector>
 #include <glm/glm.hpp>
+#include <list>
 #include "Fighter.h"
+
+class ParticleManager;
+class Emitter 
+{
+public:
+    Emitter() { }
+    Emitter* setParticleLifetime(float l);
+    Emitter* setLocation(glm::vec3 l);
+    Emitter* setOutputRate(float r); // how many particles are output per second
+private:
+    float lifetime_;
+    glm::vec3 loc_;
+    glm::vec3 vel_;
+    float rate_;
+    friend class ParticleManager;
+};
+
+struct Particle
+{
+    glm::vec3 loc;
+    glm::vec3 vel;
+    float t; // how much longer will it remain alive? 
+};
+
+
 class ParticleManager 
 {
 public:
@@ -11,7 +37,11 @@ public:
     static ParticleManager* get();
 
     void render(float dt);
-
+    Emitter* newEmitter();
+    void addEmitter(Emitter*);
+private:
+    std::vector<Emitter*> emitters;
+    std::list<Particle*> particles;
 
 };
 
