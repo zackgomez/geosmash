0368c1525bab0aebc3d8057d43f48dab0e009161
Zack Gomez <zacharyg@caltech.edu>
2012-03-04 16:59:23 -0800

    Reverted to original sphere background

--- shaders/sphere.v.glsl
+++ shaders/sphere.v.glsl
@@ -14,31 +14,6 @@ void main()
    // between 0 and 2*pi
    coord = position.xy;
 
-   float u = (coord.x + 5) / 10 * 2 * pi;
-   float v = (coord.y + 5) / 10 * 6 * pi;
-
-   // wobbly sphere
-   vec3 world = vec3(
-    sin(u) * sin(v) + 0.05 * sin(t) * cos(4 * v),
-    cos(v),
-    cos(u) * sin(v) + 0.05 * sin(t) * cos(4 * u));
-
-   /*
-   vec3 world = vec3(
-       sin(u+t) * (cos(v) + 2),
-       v - 3 + 0.4 * sin(2 * t),
-       cos(u+t) * (cos(v) + 2));
-       */
-
-   //world.y *= 2;
-
-   gl_Position = projectionMatrix * modelViewMatrix * vec4(world, 1.0);
-
-
-
-
-
-   /*
    float u = (position.x + 5.0) / 10.0 * 2 * pi;
    float v = (position.y + 5.0) / 10.0 * 2 * pi;
 
@@ -46,6 +21,5 @@ void main()
    //world = 0.01*vec3(position.x, 0.f, position.y);
 
    gl_Position = projectionMatrix * modelViewMatrix * vec4(world,1.0);
-   */
 }
 
